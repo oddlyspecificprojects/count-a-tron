9a226d9a230a7e1fba10d1364a7ac59f006a58c9
OddlySpecificProjects <noamw2000@protonmail.com>
2022-06-18 17:28:48 +0300

    More seperation

--- src/led_control.c
+++ src/led_control.c
@@ -6,7 +6,12 @@ extern DMA_HandleTypeDef hdma_tim2_ch1;
 // each data unit is a bit, 3 bytes per led is 24
 static uint8_t s_timer_led_data[LED_COUNT * 24] = {0};
 
-
+/**
+ * @brief Convert a byte to WS2812 timings
+ * 
+ * @param timing timing buffer
+ * @param data byte to convert
+ */
 static void convert_byte_to_timings(uint8_t * timing, uint8_t data)
 {
 	for (size_t i = 0; i < 8; i++)
@@ -22,25 +27,33 @@ static void convert_byte_to_timings(uint8_t * timing, uint8_t data)
 	}
 }
 
-
-static void convert_led_to_timings(uint8_t * timing, const LED_t * leds, size_t led_count)
+/**
+ * @brief Convert a single instance of led to timings
+ * 
+ * @param timing timing buffer
+ * @param led led to convert
+ */
+static void convert_led_to_timings(uint8_t * timing, const LED_t led)
 {
-	for (size_t i = 0; i < led_count; i++)
-	{
-		convert_byte_to_timings(timing + 24 * i, leds[i].green);
-		convert_byte_to_timings(timing + 24 * i + 8, leds[i].blue);
-		convert_byte_to_timings(timing + 24 * i + 16, leds[i].red);
-	}
+	convert_byte_to_timings(timing, led.green);
+	convert_byte_to_timings(timing + 8, led.blue);
+	convert_byte_to_timings(timing + 16, led.red);
 	
 }
 
 
 int write_leds(const LED_t * leds, size_t led_count)
 {
-	if (TIM_CHANNEL_STATE_GET(&htim2, TIM_CHANNEL_1) == HAL_BUSY)
+	if (TIM_CHANNEL_STATE_GET(&htim2, TIM_CHANNEL_1) == HAL_TIM_CHANNEL_STATE_BUSY)
 	{
 		return -1;
 	}
-	convert_led_to_timings(s_timer_led_data, leds, led_count);
+	for (size_t i = 0; i < led_count; i++)
+	{
+		convert_led_to_timings(s_timer_led_data + 24 * i, leds[i]);
+	}
+	
+	
 	HAL_TIM_PWM_Start_DMA(&htim2, TIM_CHANNEL_1, (uint32_t*)s_timer_led_data, sizeof(s_timer_led_data));
+	return 0;
 }
